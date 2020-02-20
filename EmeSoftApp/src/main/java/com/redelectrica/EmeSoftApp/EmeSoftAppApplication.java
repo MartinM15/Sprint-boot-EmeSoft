08f1ec1bc3f64d9bf9d5f0004d029bad98c5fd42
MartinM15 <jmmurilloj@hotmail.com>
2020-02-20 11:33:47 -0600

    Commit initi with eclipse. close #1

---
+++ EmeSoftApp/src/main/java/com/redelectrica/EmeSoftApp/EmeSoftAppApplication.java
@@ -0,0 +1,13 @@
+package com.redelectrica.EmeSoftApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmeSoftAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmeSoftAppApplication.class, args);
+	}
+
+}
---
+++ EmeSoftApp/src/test/java/com/redelectrica/EmeSoftApp/EmeSoftAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redelectrica.EmeSoftApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmeSoftAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
